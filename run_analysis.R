a26e3bbc54b47624854243c49a0565636d70d58e
Rishabh Shukla <rish.shu29@gmail.com>
2015-09-27 23:32:53 +0530

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,8 @@
  
  
- ## Download and unzip the dataset:
+
  
+##Function for downloading the require file
  loadingFile <- function(){
                  
                  filename <- "getdata_dataset.zip"
@@ -17,7 +18,7 @@
                  
  }
 
-
+## Main function which will do the functionality
 run_analysis <- function(){
                 
                 library(tools)
@@ -31,34 +32,42 @@ run_analysis <- function(){
                 features <- read.table("UCI HAR Dataset/features.txt")
                 features[,2] <- as.character(features[,2])
                 
+		##Taking X_test from test data set and train and rbind them
                 data_X_Test <- rbind(read.table("UCI HAR Dataset/train/X_train.txt", 
                                            colClasses = "numeric"), 
                                 read.table("UCI HAR Dataset/test/X_test.txt", 
                                            colClasses = "numeric"))
-                
+
+                ##Taking Y_test from test data set and train data and rbind them
                 data_Y_Test <- rbind(read.table("UCI HAR Dataset/train/y_train.txt", 
                                                  colClasses = "numeric"), 
                                       read.table("UCI HAR Dataset/test/y_test.txt", 
                                                  colClasses = "numeric"))
+
+		##Taking subject_test from test data set and train data and rbind them
                 data_Subject <- rbind(read.table("UCI HAR Dataset/train/subject_train.txt", 
                                                colClasses = "numeric"),
                                     read.table("UCI HAR Dataset/test/subject_test.txt", 
                                                colClasses = "numeric"))
 
+		##Taking only those data which have mean and standard deviation.
                 tempFeatures <- grep(".*mean.*|.*std.*", features[,2])
                 data_X_Test <- data_X_Test[, tempFeatures]
                 
 #                 activityPath <- list.files(recursive = TRUE, pattern="activity_labels.txt")
 #                 activities <- read.table(activityPath, col.names=c("activityID", "activityName"), stringsAsFactors=FALSE)
                 
+		##Labeling the variable.
                 names(data_Subject)[1] <- "subject"
                 names(data_Y_Test)[1] <- "activity"
                 names(data_X_Test)<-features[tempFeatures, 2]
                 myData <- cbind(data_Subject, data_Y_Test, data_X_Test)
                 
+		##Factoring the data
                 myData$activity <- factor(myData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
                 myData$subject <- as.factor(myData$subject)
                 
+		## Melting the data
                 myData.melted <- melt(myData, id = c("subject", "activity"))
                 myData.mean <- dcast(myData.melted, subject + activity ~ variable, mean)
                 
